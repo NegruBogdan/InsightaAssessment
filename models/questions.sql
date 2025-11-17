0a1d511cf3e7977487ac44588b0558bef6ddb309
Negru Liviu-Bogdan <bnegru68@gmail.com>
2025-11-17 02:10:03 +0200

    Update questions.sql

--- models/questions.sql
+++ models/questions.sql
@@ -28,4 +28,5 @@ left join {{ ref('tags') }} t
     on trim(tag_name) = trim(t.tag_name)
 where t.tag_id is not null
 and r.question_id is not null
+and r.owner_user_id is not null
 group by 1,2,3,4,5,6,7
